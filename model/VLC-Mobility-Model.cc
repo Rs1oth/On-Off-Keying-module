0908c439faf5dbdf46f5742e738d858766290a28
JamesBasuino <jb392@njit.edu>
2015-07-20 14:47:06 -0400

    Update VLC-Mobility-Model.cc

--- model/VLC-Mobility-Model.cc
+++ model/VLC-Mobility-Model.cc
@@ -1,20 +1,10 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
+
+/* 
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Author: Gustavo Carneiro  <gjc@inescporto.pt>
- */
+ * Author: Ryan Ackerman   <rea9@njit.edu>
+ *         
+ *         
+*/
 #include "VLC-Mobility-Model.h"
 #include "ns3/enum.h"
 #include "ns3/double.h"
